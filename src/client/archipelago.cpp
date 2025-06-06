413bbccdd9690a2213a81b0cc0ced9d9bc7ef391
Axertin <oldsgolden@gmail.com>
2025-06-06 02:24:32 -0500

    Add archipelago socket logic

--- src/client/archipelago.cpp
+++ src/client/archipelago.cpp
@@ -1,17 +1,141 @@
 
-#define NOMINMAX
-
 #include "pch.h"
+#include "archipelago.h"
 
 #pragma warning(push, 0)
-// #include <apclient.hpp>
+#include <apuuid.hpp>
 #pragma warning(pop)
 
-DWORD clientInit()
+#pragma warning(push)
+#pragma warning(disable : 4996)
+const std::string uuid_file = std::string(std::getenv("APPDATA")).append("\\uuid");
+#pragma warning(pop)
+
+const std::string CertStore = "cacert.pem";
+
+const std::string GameName = "Ōkami HD";
+const int ItemHandlingVector = 0b111;
+
+static APClient *ap = nullptr;
+std::string uuid = "";
+std::string Seed = "";
+std::string ConnectionStatus = "";
+bool IsHTTPS = false;
+bool IsWSS = false;
+bool IsWS = false;
+bool APConnected = false;
+bool APSyncQeued = false;
+
+void clientInit()
 {
-    return 0;
 }
 
-void archipelagoClientThread()
+void clientConnect(ConnectionInfo Connection)
 {
+    uuid = ap_get_uuid(uuid_file,
+                       (Connection.URI.empty()) ? APClient::DEFAULT_URI : IsWS ? Connection.URI.substr(5)
+                                                                      : IsWSS  ? Connection.URI.substr(6)
+                                                                               : Connection.URI);
+
+    if (ap != nullptr)
+        ap->reset();
+
+    ap = new APClient(uuid, GameName, Connection.URI, CertStore);
+    APSyncQeued = false;
+
+    ap->set_slot_connected_handler([Connection](const nlohmann::json &Data)
+                                   {
+                                       ConnectionStatus = "Connected";
+                                       // bool DeathLink = false;
+                                       std::list<std::string> Tags = {};
+                                       // if(DeathLink)
+                                       //     Tags.push_back("DeathLink");
+                                       ap->ConnectUpdate(false, ItemHandlingVector, true, Tags);
+                                       ap->StatusUpdate(APClient::ClientStatus::PLAYING);
+
+                                       ; // TODO: Do other things on connection
+                                   });
+    ap->set_slot_disconnected_handler([Connection]() {});
+
+    ap->set_slot_refused_handler([Connection](const std::list<std::string> &Errors)
+                                 {
+                                    for (const auto& error : Errors) {
+                                        ; // TODO: Do something when refused a slot
+                                    } });
+
+    ap->set_room_info_handler([Connection]()
+                              {
+                                std::list<std::string> Tags;
+                                ap->ConnectSlot(Connection.Slot, Connection.Password, ItemHandlingVector, Tags); });
+
+    ap->set_items_received_handler([](const std::list<APClient::NetworkItem> &Items)
+                                   {
+                                    for (const auto& Item : Items) {
+                                        ; // TODO: Do something with the items we get
+                                    } });
+
+    ap->set_location_info_handler([](const std::list<APClient::NetworkItem> &Items)
+                                  {
+                                      ; // TODO: Do Something to handle locations
+                                  });
+}
+
+void sendLocation(int64_t LocationID)
+{
+    std::list<int64_t> Check;
+    Check.push_back(LocationID);
+    ap->LocationChecks(Check);
+}
+
+void gameFinished()
+{
+    ap->StatusUpdate(APClient::ClientStatus::GOAL);
+}
+
+void pollSocket()
+{
+    if (ap)
+    {
+        ap->poll();
+        ; // TODO: Do something with polled data
+    }
+}
+
+std::string getItemName(int64_t ID, int Player)
+{
+    if (ap)
+    {
+        return ap->get_item_name(ID, ap->get_player_game(Player));
+    }
+    else
+    {
+        return "";
+    }
+}
+
+std::string getItemDesc(int player)
+{
+    if (ap)
+    {
+        return "Item for " + ap->get_player_alias(player) + " playing " + ap->get_player_game(player);
+    }
+    return "";
+}
+
+std::string getAddrInfo()
+{
+    if (ap)
+    {
+        return ap->get_slot() + "_" + ap->get_seed();
+    }
+    return "";
+}
+
+bool scoutLocations(std::list<int64_t> Locations, int CreateAsHint)
+{
+    if (ap)
+    {
+        return ap->LocationScouts(Locations, CreateAsHint);
+    }
+    return false;
 }
--- src/client/archipelago.h
+++ src/client/archipelago.h
@@ -1,3 +1,20 @@
 #pragma once
+#include "pch.h"
 
-void archipelagoClientThread();
+struct ConnectionInfo
+{
+    std::string URI;
+    std::string Password;
+    std::string Slot;
+};
+
+void clientInit(void);
+void clientConnect(void);
+void sendLocation(int64_t LocationID);
+void gameFinished(void);
+void pollSocket(void);
+
+std::string getItemName(int64_t ID, int Player);
+std::string getItemDesc(int player);
+std::string getAddrInfo();
+bool scoutLocations(std::list<int64_t> Locations, int CreateAsHint);
